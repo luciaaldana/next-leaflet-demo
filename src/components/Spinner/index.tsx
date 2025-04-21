861d6fd3d5db2f2c9796733efd443308e279408d
luciaaldana <luciaaldanacastillo@gmail.com>
2025-04-21 09:32:15 -0300

    feat: probando auto bump

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -2,7 +2,7 @@ const Spinner = (): JSX.Element => (
   <div role="status">
     <svg
       aria-hidden="true"
-      className="inline w-8 h-8 text-gray-100 animate-spin dark:text-gray-600 fill-purple-700"
+      className="inline w-8 h-8 text-gray-100 animate-spin dark:text-gray-600 fill-purple-800"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
